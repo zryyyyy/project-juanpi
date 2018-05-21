2c48ef0a9c4f9528274147d4cb756b15ab01d650
zryyyyy <760659355@qq.com>
2018-05-21 21:59:34 +0800

    第七次上传

--- script/shopcar.js
+++ script/shopcar.js
@@ -11,29 +11,35 @@ define(["jquery","jquerycookie"],function(){
     shopcarlist.prototype = {
         constructor:shopcarlist,
         init(){
+             this.count = 0;
+           
             this.loading()
             .then(function(res){
                 this.jsdata = res.list;
                  console.log(this.jsdata)
                 this.render();
-              
+               this.allselect();
             }.bind(this));
+
+           
+            
         },
         loading() {
         var opt = { url: this.url };
         return $.ajax(opt);
         },
         render(){
-         
             var html = "";
+            
             var shopcarstring = $.cookie("addcar");
             var shopcararray = JSON.parse(shopcarstring);
-            $.each(shopcararray,function(index1,item1){
-         
+            $.each(shopcararray,function(index1,item1){         
                 $.each(this.jsdata,function(index2,item2){
-                    
+                   
                     if(item1.id == item2.goods_id){
-                        console.log(item1.id,item2.goods_id)
+                        var totalprice = item2.cprice*item1.num;                     
+                        this.count += totalprice*1;
+                      
                        html += `<div class="listtop">
                                     <div class="yuanwrap">
                                         <span class="yuan clear1"></span>
@@ -67,24 +73,68 @@ define(["jquery","jquerycookie"],function(){
                                             <span class="jia">+</span>
                                         </div>
                                         <div class="total pub">
-                                            ￥${item2.cprice}
+                                            ￥<a>${totalprice}</a>
                                         </div>
                                         <div class="slice pub">
                                             <span class="shanchu"></span>
                                         </div>
                                     </div>
                                 </div>`;
+                                 
+                    }  
+                  
+                    
+                }.bind(this))              
+                   
+            }.bind(this))
+            
+             $(".carttop").append(html);
+            
+                   
+        },
+        allselect(){
+            var shopcarstring = $.cookie("addcar");
+            var shopcararray = JSON.parse(shopcarstring);
+            var sum = 0; 
+            var sum2 =0;
+            var num = Number($(".shuzi").html());
+            
+         
+           
+            $.each(shopcararray,function(index,item){           
+                sum += Number(item.num);                 
+            })   
+            $(".quanwrap span").click(function() {
+                $(".yuan").toggleClass("check");
+                if ($(".quanwrap span").hasClass("check")) {
+                    $(".totalnum").html(sum); 
+                    $(".totalmsg a").html(this.count);
+                    if(this.count>=99){
+                        $(".zongjia a").html(this.count-20);
                     }
                     
+                }
+                if (!$(".quanwrap span").hasClass("check")) {
+                    $(".totalnum").html(sum2);
+                     $(".totalmsg a").html(sum2);
+                     $(".zongjia a").html(sum2);
+                }
+            }.bind(this));  
+             $(".jia").click(function(){
+                num++;
+                $(this).prev().html(num);
+                var shopcarstring = $.cookie("addcar")
+                var shopcararray = JSON.parse(shopcarstring);
+                $.each(shopcararray,function(index,item){
+                  
+                    item.num = num; 
                 })
-               
-            }.bind(this))
-             $(".carttop").append(html);
-
+                 $.cookie("addcar", JSON.stringify(shopcararray));   
+             })
+            
+        
+          
+           
         }
-
     }
-
-
-
 })
